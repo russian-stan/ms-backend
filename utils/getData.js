d21d88119c21e99a57ea6f3b0dda4dd385bdcb3a
russian-stan <stanislaw_@inbox.ru>
2020-03-19 17:27:01 +0300

    AppError in axios

--- utils/getData.js
+++ utils/getData.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
+const AppError = require('../utils/appError');
 
 module.exports = async (url) => {
   try {
     const resp = await axios.get(url);
     return resp.data;
   } catch (err) {
-    throw new Error(err.message);
+    throw new AppError(err.message, err.response.status);
   }
 };
